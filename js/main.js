058640ec39f75bb411e56ac0cd67e3610ac0ec00
ramjanss <ramjansocial@gmail.com>
2026-02-14 15:36:54 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+async function testConnection(){
+  try{
+    await getDocs(collection(db,"test"));
+    console.log("Firebase Connected Successfully");
+  }catch(e){
+    console.log("Firebase Error:",e);
+  }
+}
+
+testConnection();
