f95cffd1f28363f9a2edd310b3a49cf66bf89242
ramjanss <ramjansocial@gmail.com>
2026-02-14 17:02:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-console.log("MAIN JS LOADED");
-
 import { db } from "./firebase.js";
-import { collection, getDocs } from 
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, getDocs } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-async function testConnection(){
-  try{
-    await getDocs(collection(db,"test"));
-    console.log("Firebase Connected Successfully");
-  }catch(e){
-    console.log("Firebase Error:",e);
-  }
+const noticeList = document.getElementById("noticeList");
+
+async function loadNotices(){
+  noticeList.innerHTML = "";
+
+  const querySnapshot = await getDocs(collection(db, "notices"));
+
+  querySnapshot.forEach((docSnap) => {
+    const li = document.createElement("li");
+    li.textContent = docSnap.data().title;
+    noticeList.appendChild(li);
+  });
 }
 
-testConnection();
+loadNotices();
