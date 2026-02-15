16e3570fab175db82a7082342817249a71086880
ramjanss <ramjansocial@gmail.com>
2026-02-15 18:39:15 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // ================= IMPORTS =================
 import { db } from "./firebase.js";
 
-import {
+{
   collection,
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
